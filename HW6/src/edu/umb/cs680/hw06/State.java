be1a2eb209f3530f50997012ca579936de099e08
divya2648 <60254740+divya2648@users.noreply.github.com>
2021-11-07 19:43:35 -0500

    Delete HW6 directory

--- HW6/src/edu/umb/cs680/hw06/State.java
+++
@@ -1,8 +0,0 @@
-package edu.umb.cs680.hw06;
-
-public interface State {
-	DVDPlayer dvdPlayer = DVDPlayer.getInstance();
-	void playButtonPushed();
-    void openCloseButtonPushed();
-    void stopButtonPushed();
-}
